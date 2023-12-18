4d71e657d09e087578d5f2e9c7603eeb3fd78f8a
sajibsaifullah <dusajibphy@gmail.com>
2023-12-18 19:50:58 +0600

    swiper

---
+++ components/swiper/swiper.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
