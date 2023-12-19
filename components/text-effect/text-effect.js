0dbd57b3366e02bf2891c2c509378d1060f7fea7
sajibsaifullah <dusajibphy@gmail.com>
2023-12-20 00:20:00 +0600

    text

---
+++ components/text-effect/text-effect.js
@@ -0,0 +1,10 @@
+window.ityped.init(document.querySelector(".ityped"), {
+  strings: [
+    "I am a Passionate Web Developer.",
+    "I Love HTML5.",
+    "I Love CSS3.",
+    "I Love JavaScript.",
+    "I Love Jquery.",
+    "I Love React.",
+  ],
+});
